7ed3f2a0c6437368e409e5401c28eee8bf803579
Daurova <147259989+Daurova@users.noreply.github.com>
2025-09-20 11:45:44 +0300

    fixed eslint

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -20,7 +20,7 @@ const isPrime = (number) => {
 }
 
 const askQuestion = () => {
-  const num = Math.round(Math.random()*99)+1
+  const num = Math.round(Math.random() * 99) + 1
   const answer = readlineSync.question(`Question ${num}\n Your answer: `)
   const res = isPrime(num)
   return [res, answer]

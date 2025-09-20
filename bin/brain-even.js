9e90ab7b2297ba1bf7da58d49414990e425cfb0f
Daurova <147259989+Daurova@users.noreply.github.com>
2025-09-20 15:27:56 +0300

    fixed bug brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
 import greetUser from '../src/cli.js'
 import readlineSync from 'readline-sync'
 
 const explainRules = () => {
-  // eslint-disable-next-line no-undef
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
 }
 
@@ -19,24 +19,28 @@ const checkAnswer = () => {
   while (counter < 3) {
     let [num, answer] = askQuestion()
     if (num % 2 === 0 && answer.toLowerCase() === 'yes') {
-    // eslint-disable-next-line no-undef
       console.log ('Correct!')
       counter = counter + 1
     }
     if (num % 2 === 1 && answer.toLowerCase() === 'no') {
-    // eslint-disable-next-line no-undef
       console.log ('Correct!')
 
       counter = counter + 1
     }
-    else if (answer.toLowerCase() !== 'yes' && answer.toLowerCase() !== 'yes') {
-      // eslint-disable-next-line no-undef
+    else if (answer.toLowerCase() !== 'yes' && answer.toLowerCase() !== 'no') {
+      console.log (`Let's try again, ${user}!`)
+      return
+    }
+    else if (num % 2 === 0 && answer.toLowerCase() === 'no') {
+      console.log (`Let's try again, ${user}!`)
+      return
+    }
+    else if (num % 2 === 1 && answer.toLowerCase() === 'yes') {
       console.log (`Let's try again, ${user}!`)
       return
     }
   }
   if (counter === 3) {
-    // eslint-disable-next-line no-undef
     console.log(`Congratulations, ${user}!`)
   }
 }
